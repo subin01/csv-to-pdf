44c9d22e337773e4376448dba4f144522d3f455b
Subin Paul <subin01@gmail.com>
2021-03-29 21:09:32 +1100

    Initial commit

---
+++ src/Uploader.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "react-dropzone-uploader/dist/styles.css";
+import Dropzone from "react-dropzone-uploader";
+import Papa from "papaparse";
+
+const Uploader = ({ onComplete, maxFiles = 6 }) => {
+  let csvData = [];
+
+  // called every time a file's `status` changes
+  const handleChangeStatus = ({ meta, file }, status) => {
+    // console.log(status, meta, file);
+    if (status === "done") {
+      Papa.parse(file, {
+        worker: true,
+        header: true,
+        complete: function (results) {
+          csvData = [...csvData, ...results.data];
+          console.table(results.data);
+        },
+      });
+    }
+  };
+
+  const handleClick = () => {
+    console.log("csvData:", JSON.stringify(csvData));
+    onComplete(csvData);
+  };
+
+  return (
+    <>
+      <Dropzone
+        onChangeStatus={handleChangeStatus}
+        accept=".csv"
+        autoUpload={false}
+        maxFiles={maxFiles}
+        styles={{
+          dropzone: { width: 500, height: maxFiles * 50 },
+          dropzoneActive: { borderColor: "green" },
+        }}
+      />
+
+      <div>
+        <button onClick={handleClick}>Next</button>
+      </div>
+    </>
+  );
+};
+
+export default Uploader;
---
+++ src/mock_merge_data.js
@@ -0,0 +1,447 @@
+const mock_merged_data = {
+  CU0095MPS7VWCF: {
+    name: "SHWETHA JAYARAM",
+    email: "shwethabawse@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+    },
+  },
+  CU0053E2QAG19P: {
+    name: "Akhil Jayakumar",
+    email: "akhi05ajk@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0071BHHFACNH: {
+    name: "TANYA TOM",
+    email: "tan97jo@gmail.com",
+    payment: {
+      "06-Feb-2021": "4800.00",
+    },
+  },
+  CU0072SE77OOYZ: {
+    name: "Geetha Krishnamurthy",
+    email: "geeta.krishnamurthy@gmail.com",
+    payment: {
+      "06-Feb-2021": "1000.00",
+      "05-Jan-2021": "1000.00",
+    },
+  },
+  CU0040WT1VLZA4: {
+    name: "Anand Nandakumar",
+    email: "anandnandakrs@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0022HACSDWVS: {
+    name: "SAI PRIYA KUMAR",
+    email: "saipriyakumar7@gmail.com",
+    payment: {
+      "06-Feb-2021": "200.00",
+      "05-Jan-2021": "200.00",
+    },
+  },
+  CU0086MOUZRQJT: {
+    name: "BIJU P N",
+    email: "bijupalazhi@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0044JEL3K91L: {
+    name: "Rohit Prakash Nair",
+    email: "rohitnair567@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0038KYI8QB6Q: {
+    name: "Nikhil Viswanathan",
+    email: "viswanathan.nikhil@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0070BSJRVTRB: {
+    name: "Vinod P N",
+    email: "vpnmenon@gmail.com",
+    payment: {
+      "06-Feb-2021": "1000.00",
+      "05-Jan-2021": "1000.00",
+    },
+  },
+  CU0056SNRHZ8DA: {
+    name: "Divya khathuria",
+    email: "divyakhathuria99@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+    },
+  },
+  CU00575XSMUA3B: {
+    name: "Manjunath T",
+    email: "manjuagar@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0062PEUTEI8M: {
+    name: "B Goutham krishna",
+    email: "gouthamkittu3@gmail.com",
+    payment: {
+      "06-Feb-2021": "250.00",
+      "05-Jan-2021": "250.00",
+    },
+  },
+  CU0069DQDDXYML: {
+    name: "Madhusmita Behera",
+    email: "madhusmitabehera013@gmail.com",
+    payment: {
+      "06-Feb-2021": "1000.00",
+    },
+  },
+  CU00832VIVWN19: {
+    name: "Kalyani",
+    email: "kalyaninair25@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+    },
+  },
+  CU0077RU7T8L9M: {
+    name: "Nithya Sivaraman Mukkulath",
+    email: "nithya.mukkulath.1981@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0059ZWTNKBQF: {
+    name: "Ranjish Mukkulam",
+    email: "mgranjish@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0011EHXGB1XZ: {
+    name: "Deepak Pathiran Sorimuthu",
+    email: "edgedevasion@gmail.com",
+    payment: {
+      "06-Feb-2021": "1000.00",
+      "05-Jan-2021": "1000.00",
+    },
+  },
+  CU0043YAXXOKWH: {
+    name: "Vennela V N",
+    email: "vennelavnvennala@gmail.com",
+    payment: {
+      "06-Feb-2021": "250.00",
+    },
+  },
+  CU00319PGHDD9M: {
+    name: "Mukundan Nair",
+    email: "freedom.mknd@gmail.com",
+    payment: {
+      "06-Feb-2021": "200.00",
+      "05-Jan-2021": "200.00",
+    },
+  },
+  CU0022U9A4P9QE: {
+    name: "Jyothi P Pathak",
+    email: "jopathak@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0080W7DFKGD4: {
+    name: "Naren Adithya B",
+    email: "narenadithya1@gmail.com",
+    payment: {
+      "06-Feb-2021": "1000.00",
+    },
+  },
+  CU0083DWX3VPL3: {
+    name: "Tony Philip",
+    email: "thesaligian@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0083P1RC7BYZ: {
+    name: "Lakshmi S",
+    email: "papa.lakshmi.raghunath@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0078NMV7RUNQ: {
+    name: "Shyamala Gopinath",
+    email: "mangalya.gopinath.shyamala@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+    },
+  },
+  CU00729OCWL8CZ: {
+    name: "Rahul prakasan",
+    email: "rahulprakasn@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0077OTOHDHNT: {
+    name: "Prasanth Ramachandran",
+    email: "prasanth431@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+    },
+  },
+  CU0086Y4253RYE: {
+    name: "Sudhi Soman",
+    email: "sudhisooraj0915@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU00838KD9NRYV: {
+    name: "Bala Sri Harsha",
+    email: "harshasri5@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0037MRCLCKAX: {
+    name: "Akanksha Agarwal",
+    email: "akankshaagarwal0122@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0026Z7FDZRZW: {
+    name: "Nikitta Grace Oomman",
+    email: "nikitta.g.oomman@gmail.com",
+    payment: {
+      "06-Feb-2021": "3000.00",
+      "05-Jan-2021": "3000.00",
+    },
+  },
+  CU00377RMURWWC: {
+    name: "Trishala N",
+    email: "trishalanagraj2018@gmail.com",
+    payment: {
+      "06-Feb-2021": "250.00",
+    },
+  },
+  CU0068TUVRVRAZ: {
+    name: "Sujith P S",
+    email: "sujithps@live.in",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0042FEKCVRQU: {
+    name: "M Anuradha",
+    email: "gouthamkittu3@gmail.com",
+    payment: {
+      "06-Feb-2021": "1000.00",
+      "05-Jan-2021": "1000.00",
+    },
+  },
+  CU0063MFC1T8RB: {
+    name: "Malvika Jhaveri",
+    email: "jhaverimalvika@gmail.com",
+    payment: {
+      "06-Feb-2021": "1000.00",
+      "05-Jan-2021": "1000.00",
+    },
+  },
+  CU0051PYYEGFWD: {
+    name: "Rucha Umesh Deshmukh",
+    email: "deshmukh.rucha10@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0074GYDDMMEZ: {
+    name: "Almas Shaikh",
+    email: "almasshaikh94@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0014IYTMHMAV: {
+    name: "Suneesh kumar prabhu",
+    email: "suneeshprabhu@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0043XYW1IZKD: {
+    name: "Anisha Chinnu Abraham",
+    email: "anisha.abraham97@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0089WKQEUYG4: {
+    name: "Venkata Praveen Naradasu",
+    email: "guptakratika93@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0086PTUX4DQR: {
+    name: "Manasa Gundaji",
+    email: "manasa0115g@gmail.com",
+    payment: {
+      "06-Feb-2021": "250.00",
+      "05-Jan-2021": "250.00",
+    },
+  },
+  CU0072FSSOPWTM: {
+    name: "Arundhathi V A",
+    email: "arundati.5.vs@gmail.com",
+    payment: {
+      "06-Feb-2021": "250.00",
+      "05-Jan-2021": "250.00",
+    },
+  },
+  CU0075ZYZOCMNC: {
+    name: "Maryann Oliver",
+    email: "maryannolivers@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0075KG6TDCW7: {
+    name: "Aswathy S",
+    email: "aswathys0100@gmail.com",
+    payment: {
+      "06-Feb-2021": "1000.00",
+      "05-Jan-2021": "1000.00",
+    },
+  },
+  CU0049KMVRGC8B: {
+    name: "Diya Fathima M S",
+    email: "mohamedsunil@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU00693XW4EWRN: {
+    name: "Maloy Kumar Ghosh",
+    email: "maloykumarghosh0@gmail.com",
+    payment: {
+      "06-Feb-2021": "1000.00",
+      "05-Jan-2021": "1000.00",
+    },
+  },
+  CU0035KE2TU5MT: {
+    name: "Merilynn Gloria Joseph",
+    email: "merilynngj@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0072ME2F9HAH: {
+    name: "Kopraparambil Denis Cheryl",
+    email: "cherylcjc98@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+    },
+  },
+  CU0040PMZRMS2Y: {
+    name: "Akash N P",
+    email: "akashnp1998@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+    },
+  },
+  CU0085PCAWAUMT: {
+    name: "Ajith Joy",
+    email: "ajith.joy15@gmail.com",
+    payment: {
+      "06-Feb-2021": "1000.00",
+      "05-Jan-2021": "1000.00",
+    },
+  },
+  CU0065XK5HX8FB: {
+    name: "Lakshmi Anilkumar",
+    email: "lakshmikurupanilkumar@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0058BN2QRWZX: {
+    name: "Sujith S",
+    email: "sujithnair22@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+    },
+  },
+  CU0040FUZRCRDW: {
+    name: "RIAZ ABDUL MAJEED",
+    email: "riaz.abdulmajeed13@gmail.com",
+    payment: {
+      "06-Feb-2021": "2000.00",
+      "05-Jan-2021": "2000.00",
+    },
+  },
+  CU0094MVIVOGHC: {
+    name: "Karan Jacob Varghese",
+    email: "karanjacob5895@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+  CU0067V7USBWVS: {
+    name: "Nidhin Dilip",
+    email: "nidhindilip9889@gmail.com",
+    payment: {
+      "06-Feb-2021": "1000.00",
+      "05-Jan-2021": "1000.00",
+    },
+  },
+  CU0070G2BNZYSY: {
+    name: "Rajnish Deo",
+    email: "rajnishdeo99@gmail.com",
+    payment: {
+      "06-Feb-2021": "100.00",
+      "05-Jan-2021": "100.00",
+    },
+  },
+  CU0086LRVDV5AK: {
+    name: "Meera Francis",
+    email: "meerafrancichittettu@gmail.com",
+    payment: {
+      "06-Feb-2021": "500.00",
+      "05-Jan-2021": "500.00",
+    },
+  },
+};
+
+export default mock_merged_data;
